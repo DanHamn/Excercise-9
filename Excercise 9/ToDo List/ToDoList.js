4f512b0a5c5bc0f2de5819b48c0f26033e0d059f
DanHamn <85741261+DanHamn@users.noreply.github.com>
2021-08-09 15:40:44 +0200

    Add files via upload
    Fix some formatting on the ToDo list

--- Excercise 9/ToDo List/ToDoList.js
+++ Excercise 9/ToDo List/ToDoList.js
@@ -3,80 +3,88 @@ const myList = document.getElementsByTagName("LI");
 for (let i = 0; i < myList.length; i++) {
     appendX(myList[i]);
 }
-//Makes clicks on the x remove the item from the list
-const close = document.getElementsByClassName("close");
-makeXWork()
 
 //Adds the submited text to the list and appends an remove "x" at the end
-const submit = document.querySelector('#inputToDoList');
-submit.addEventListener('submit', e => {
-    e.preventDefault();
+document.querySelector('#inputToDoList')
+    .addEventListener('submit', e => {
+        e.preventDefault();
 
-    let li = document.createElement("li");
+        let li = document.createElement("li");
+        let inputValue = document.getElementById("addToList").value;
+        let t = document.createTextNode(inputValue);
+        li.appendChild(t);
+        if (inputValue == '') {
+            alert("You must write something!");
+        }
+        else {
+            document.getElementById("theToDoList").appendChild(li);
+        }
+        document.getElementById("addToList").value = "";
 
-    let inputValue = document.getElementById("addToList").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue == '') {
-        alert("You must write something!");
-    }
-    else {
-        document.getElementById("theToDoList").appendChild(li);
-    }
-    document.getElementById("addToList").value = "";
+        appendX(li);
 
-    appendX(li);
-
-    makeXWork()
-
-})
+    })
 
 function appendX(List) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
     span.className = "close border";
     span.appendChild(txt);
     List.appendChild(span);
-}
 
-function makeXWork() {
+    //Makes clicks on the x remove the item from the list
+    const close = document.getElementsByClassName("close");
     for (i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            var div = this.parentElement;
-            div.parentNode.removeChild(div);
-        }
+        close[i].addEventListener('click', function () {
+            this.parentElement.remove();
+        })
     }
 }
+
 
 //toggles the "checked" status for the line the curser clicks on
-const list = document.querySelector('ul');
-list.addEventListener('click', e => {
-    if (e.target.tagName == 'LI') {
-        e.target.classList.toggle('checked');
-    }
-});
+document.querySelector('ul')
+    .addEventListener('click', e => {
+        if (e.target.tagName == 'LI') {
+            e.target.classList.toggle('checked');
+        }
+    });
+
+
 
 //Toggles the "checked" status for all lines in the list
-const toggle = document.getElementById('markUnmarkAllBtn').addEventListener('click', function () {
-    let elem = document.getElementById("markUnmarkAllBtn");
-    if (elem.innerHTML == "Markera alla") {
-        let element = document.querySelectorAll('#nav>ul>li');
-        for (let index = 0; index < element.length; index++) {
-            element[index].classList.add('checked');
+document.getElementById('markUnmarkAllBtn').addEventListener('click', function () {
+    let markBtm = document.getElementById("markUnmarkAllBtn");
+    let listItems = document.querySelectorAll('#nav>ul>li');
+    if (markBtm.innerHTML == "Markera alla") {
+        for (let index = 0; index < listItems.length; index++) {
+            listItems[index].classList.add('checked');
         }
     }
     else {
-        let element = document.querySelectorAll('#nav>ul>li');
-        for (let index = 0; index < element.length; index++) {
-            element[index].classList.remove('checked');
+        for (let index = 0; index < listItems.length; index++) {
+            listItems[index].classList.remove('checked');
         }
     }
     btnChange();
 })
+
 //Change the button that marks and unmarks everything in the list
 function btnChange() {
-    let elem = document.getElementById("markUnmarkAllBtn");
-    if (elem.innerHTML == "Markera alla") elem.innerHTML = "Avmarkera alla";
-    else elem.innerHTML = "Markera alla";
+    let markBtm = document.getElementById("markUnmarkAllBtn");
+    if (markBtm.innerHTML == "Markera alla") markBtm.innerHTML = "Avmarkera alla";
+    else markBtm.innerHTML = "Markera alla";
 }
 
+document.getElementById('removeAllBtn').addEventListener('click', function () {
+    let listItems = document.querySelectorAll('#nav>ul>li');
+    if (listItems.length > 0) {
+        let remove = confirm('Är du säker på att du vill töma listan?')
+        if (remove == true) {
+            for (let index = 0; index < listItems.length; index++) {
+                listItems[index].remove()
+            }
+            document.getElementById("markUnmarkAllBtn").innerHTML = "Markera alla"
+        }
+    }
+})
